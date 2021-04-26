d2ff37aef1e09e92bf70117a90fe6663f6d4b0ac
--global <dgarcia@grupomabarak.com.mx>
2021-04-26 16:13:17 -0500

    avance hasta el video 20

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -18,15 +18,46 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido(){
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched
+  }
+  get apellidoNoValido(){
+    return this.forma.get('apellido').invalid && this.forma.get('nombre').touched
+  }
+  get correoNoValido(){
+    return this.forma.get('correo').invalid && this.forma.get('nombre').touched
+  }
+  get distritoNoValido(){
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched
+  }
+
+  get ciudadNoValido(){
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched
+  }
+
   crearFormulario(){
     this.forma = this.fb.group({
-      nombre  : ['Daniel'],
-      apellido: ['Garcia'],
-      correo  : ['dalokdaga@gmail.com'],
+      nombre  : ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', [Validators.required, Validators.minLength(3)]],
+      correo  : ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],  
+      direccion:this.fb.group({
+        distrito:['', [Validators.required, Validators.minLength(3)]],
+        ciudad  :['', [Validators.required, Validators.minLength(3)]],
+      })
     });
   }
 
-  guardar() {
-    console.log(this.forma);
+  guardar( ){
+    if(this.forma.invalid){
+      // activamos los touched del formulario para
+      // mostrar los errores de validación
+      Object.values( this.forma.controls ).forEach( control =>{
+        console.log(control)
+        control.markAsTouched();
+      });
+
+      return;
+    }
+   console.log(this.forma.value)
   }
 }
