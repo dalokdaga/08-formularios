04b4181436d471d2c2b319347b13d0233a023414
--global <dgarcia@grupomabarak.com.mx>
2021-04-26 11:17:56 -0500

    avance hasat video 17

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -8,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.crearFormulario();
+  }
 
   ngOnInit(): void {
   }
 
+  crearFormulario(){
+    this.forma = this.fb.group({
+      nombre  : ['Daniel'],
+      apellido: ['Garcia'],
+      correo  : ['dalokdaga@gmail.com'],
+    });
+  }
+
+  guardar() {
+    console.log(this.forma);
+  }
 }
